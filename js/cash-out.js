156540657cd52c488a21e6f6e104f934add60927
imon-sheikh <mdimonhossain2017@gmail.com>
2024-09-22 16:16:14 +0600

    cash out function is working

--- js/cash-out.js
+++ js/cash-out.js
@@ -5,6 +5,17 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
 
     const cashOut = getInputFieldValueId('input-cash-out')
     const pinNumber = getInputFieldValueId('input-cash-out-pin')
-    console.log("inside the click handler", cashOut, pinNumber);
+    // console.log("inside the click handler", cashOut, pinNumber);
+
+   if(pinNumber === 1234){
+    const balance = getTextFieldValueById('account-balance')
+    // console.log("balance here", balance);
+    const newBalance = balance - cashOut
+    document.getElementById('account-balance').innerText = newBalance
+    // balance.innerText = newBalance
+    
+    }else{
+        alert('No money for you....DGM')
+    }
     
 })
