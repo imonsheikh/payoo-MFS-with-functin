b1e9d8686f2820e41c164d014a40b464a4bf8978
imon-sheikh <mdimonhossain2017@gmail.com>
2024-09-22 16:03:14 +0600

    added cashout function

--- js/cash-out.js
+++ js/cash-out.js
@@ -1,1 +1,10 @@
-console.log("cash out file added");
+// console.log("cash out file added");
+
+document.getElementById('btn-cash-out').addEventListener('click', function(event){
+    event.preventDefault()
+
+    const cashOut = getInputFieldValueId('input-cash-out')
+    const pinNumber = getInputFieldValueId('input-cash-out-pin')
+    console.log("inside the click handler", cashOut, pinNumber);
+    
+})
